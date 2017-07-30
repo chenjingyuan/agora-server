96cd5777975300fcf14f2816eaf6cdf85d0d04a5
yanxx <yanwei871205@163.com>
2017-07-29 23:02:16 -0700

    modify dependencies

--- src/main/java/com/melot/recorder/inferface/Kernel32.java
+++ src/main/java/com/melot/recorder/inferface/Kernel32.java
@@ -7,7 +7,7 @@ import com.sun.jna.Native;
  * Created by mn on 2017/7/17.
  */
 public interface Kernel32 extends Library {
-    public static Kernel32 INSTANCE = (Kernel32) Native.loadLibrary(Kernel32.class);
+    public static Kernel32 INSTANCE = (Kernel32) Native.loadLibrary("jna",Kernel32.class);
 
     public int GetProcessId(Long hProcess);
 }
